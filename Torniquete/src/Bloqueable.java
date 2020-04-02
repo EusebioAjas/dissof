f5f148ed86ee53649523714a25dabc6355e1abea
EusebioAjas <eusebioajas6@gmail.com>
2020-04-01 20:37:23 -0600

    Torniquete

---
+++ Torniquete/src/Bloqueable.java
@@ -0,0 +1,3 @@
+public interface Bloqueable {
+    void coin();
+}
---
+++ Torniquete/src/Estado.java
@@ -0,0 +1,15 @@
+public class Estado {
+    private Status STATUS;
+
+    public Estado(Status status) {
+        this.STATUS = status;
+    }
+
+    public Status getStatus() {
+        return STATUS;
+    }
+
+    public void setStatus(Status status) {
+        this.STATUS = status;
+    }
+}
